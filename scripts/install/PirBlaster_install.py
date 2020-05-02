2e6facc59baa9649b1b9a70c726168549a669911
jbacon <julien.bacon.1@gmail.com>
2020-05-02 14:35:09 -0400

    correcting installation script mishap

--- scripts/install/PirBlaster_install.py
+++ scripts/install/PirBlaster_install.py
@@ -32,49 +32,97 @@ def execCommand(command):
     process = subprocess.run(command.split(' '))
     return process.returncode
 
-# Install PIGPIO
-def installPIGPIO():
-    # TODO: Check if sevice is already installed & Split in multiple functions
-    print(f"{Text.HEADER}*** INSTALLING PIGPIO SERVICE ***{Text.ENDC}")
+# Download PIGPIO
+def downloadPIGPIO():
+    print(f"{Text.HEADER}*** DOWNLOADING PIGPIO ***{Text.ENDC}")
     cmdResult = execCommand(Command.DWNLD_PIGPIO)
     if cmdResult != 0:
         print(f"{Text.FAIL}PIGPIO DOWNLOAD FAILED!!!{Text.ENDC}")
         return False
     print(f"{Text.SUCCESS}PIGPIO DOWNLOAD DONE{Text.ENDC}")
+    return True
+
+# Unzip PIGPIO
+def unzipPIGPIO():
+    print(f"{Text.HEADER}*** UNZIPPNG PIGPIO ***{Text.ENDC}")
     cmdResult = execCommand(Command.UNZIP_PIGPIO)
     if cmdResult != 0:
         print(f"{Text.FAIL}PIGPIO UNZIP FAILED!!!{Text.ENDC}")
         return False
     print(f"{Text.SUCCESS}PIGPIO UNZIP DONE{Text.ENDC}")
+    return True
+
+# Building PIGPIO
+def buildPIGPIO():
+    print(f"{Text.HEADER}*** BUILDING PIGPIO ***{Text.ENDC}")
     os.chdir('pigpio-master')
     cmdResult = execCommand(Command.BUILD_PIGPIO)
     if cmdResult != 0:
         print(f"{Text.FAIL}PIGPIO BUILD FAILED!!!{Text.ENDC}")
         return False
     print(f"{Text.SUCCESS}PIGPIO BUILD DONE{Text.ENDC}")
+    return True
+
+# Install PIGPIO
+def installPIGPIO():
+    print(f"{Text.HEADER}*** INSTALLING PIGPIO ***{Text.ENDC}")
     cmdResult = execCommand(Command.INSTALL_PIGPIO)
     if cmdResult !=0:
         print(f"{Text.FAIL}PIGPIO INSTALL FAILED!!!{Text.ENDC}")
         return False
     print(f"{Text.SUCCESS}PIGPIO INSTALL DONE{Text.ENDC}")
+    return True
+
+# Creating PIGPIO service
+def createPigpioSvc():
+    print(f"{Text.HEADER}*** CREATING PIGPIO SERVICE ***{Text.ENDC}")
     os.chdir('..')
     cmdResult = execCommand(Command.CREATE_PIGPIO_SVC)
     if cmdResult != 0:
-        print(f"{Text.FAIL}PIGPIOD SETUP FAILED!!!{Text.ENDC}")
+        print(f"{Text.FAIL}CREATING PIGPIO SERVICE FAILED!!!{Text.ENDC}")
         return False
+    print(f"{Text.SUCCESS}CREATING PIGPIO SERVICE DONE{Text.ENDC}")
+    return True
+
+# Enabling PIGPIO service
+def enablePigpioSvc():
+    print(f"{Text.HEADER}*** ENABLING PIGPIO SERVICE ***{Text.ENDC}")
     cmdResult = execCommand(Command.ENABLE_PIGPIO_SVC)
     if cmdResult != 0:
-        print(f"{Text.FAIL}PIGPIOD SETUP FAILED!!!{Text.ENDC}")
+        print(f"{Text.FAIL}ENABLING PIGPIO SERVICE FAILED!!!{Text.ENDC}")
         return False
+    print(f"{Text.SUCCESS}ENABLING PIGPIO SERVICE DONE{Text.ENDC}")
+    return True
+
+# Starting PIGPIO service
+def startPigpioSvc():
+    print(f"{Text.HEADER}*** STARTING PIGPIO SERVICE ***{Text.ENDC}")
     cmdResult = execCommand(Command.START_PIGPIO_SVC)
     if cmdResult != 0:
-        print(f"{Text.FAIL}PIGPIOD SETUP FAILED!!!{Text.ENDC}")
+        print(f"{Text.FAIL}STARTING PIGPIO SERVICE FAILED!!!{Text.ENDC}")
         return False
-    print(f"{Text.SUCCESS}PIGPIOD SETUP DONE{Text.ENDC}")
+    print(f"{Text.SUCCESS}STARTING PIGPIO SERVICE DONE{Text.ENDC}")
     return True
+
+# Setup PIGPIO service
+def setupPigpioSvc():
+    # TODO: Check if sevice is already installed & Split in multiple functions
+    print(f"{Text.HEADER}*** SETTING UP PIGPIO SERVICE ***{Text.ENDC}")
+    if downloadPIGPIO():
+        if unzipPIGPIO():
+            if buildPIGPIO():
+                if installPIGPIO():
+                    if createPigpioSvc():
+                        if enablePigpioSvc():
+                            if startPigpioSvc():
+                                print(f"{Text.SUCCESS}SETTING UP PIGPIO SERVICE DONE{Text.ENDC}")
+                                return True
+    print(f"{Text.FAIL}SETTING UP PIGPIO SERVICE FAILED!!!{Text.ENDC}")
+    return False
 
 # Clone PirBlaster repo
 def clonePirBlaster():
+    # TODO: Copuing the deployment key
     print(f"{Text.HEADER}*** CLONING PIRBLASTER REPO ***{Text.ENDC}")
     cmdResult = execCommand(Command.CLONE_PIRBLASTER)
     if cmdResult != 0:
@@ -154,11 +202,10 @@ def startPirBlasterSvc():
     print(f"{Text.SUCCESS}STARTING PIRBLASTER SERVICE DONE{Text.ENDC}")
     return True
 
-# print(f"{Text.HEADER}*** SERVICE CONFIGURATION ***{Text.ENDC}")
-# Ask for the hostname the service will use for advertising
-# hostname = input(f"Please enter the hostname that the service will use for advertising:")
-
-if clonePirBlaster():
+# Setup PirBlaster service
+def setupPirBlasterSvc():
+    # TODO: Check if sevice is already installed
+    print(f"{Text.HEADER}*** SETTING UP PIRBLASTER SERVICE ***{Text.ENDC}")
     if createVirtualEnv():
         if activateVirutalEnv():
             if installDependencies():
@@ -166,6 +213,18 @@ if clonePirBlaster():
                     if createPirBlasterSvc():
                         if enablePirBlasterSvc():
                             if startPirBlasterSvc():
-                                print(f"{Text.SUCCESS}INSATALLING PIRBLASTER SERVICE DONE{Text.ENDC}")
-                                exit()
+                                print(f"{Text.SUCCESS}SETTING UP PIRBLASTER SERVICE DONE{Text.ENDC}")
+                                return True
+    print(f"{Text.FAIL}SETTING UP PIRBLASTER SERVICE FAILED!!!{Text.ENDC}")
+    return False
+
+# print(f"{Text.HEADER}*** SERVICE CONFIGURATION ***{Text.ENDC}")
+# Ask for the hostname the service will use for advertising
+# hostname = input(f"Please enter the hostname that the service will use for advertising:")
+
+if clonePirBlaster():
+    if setupPigpioSvc():
+        if setupPirBlasterSvc():
+            print(f"{Text.SUCCESS}INSATALLING PIRBLASTER SERVICE DONE{Text.ENDC}")
+            exit()
 print(f"{Text.FAIL}INTALLING PIRBLASTER SERVICE FAILED!!!{Text.ENDC}")
