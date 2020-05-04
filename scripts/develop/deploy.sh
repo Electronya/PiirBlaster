a0cb9dc5a78b78f2c94a6f71221edeb3813e89e5
jbacon <julien.bacon.1@gmail.com>
2020-05-03 20:48:43 -0400

    updating deploy script for service suport

--- scripts/develop/deploy.sh
+++ scripts/develop/deploy.sh
@@ -1,4 +1,63 @@
 #!/bin/bash
 
-ssh labPi 'test -d ~/PirBlasterBackend || mkdir ~/PirBlasterBackend'
-rsync -avz ./ labPi:PirBlasterBackend
+function isServiceInstalled() {
+    echo -e "\e[1;123m*** TEST IF SERVICE IS INSTALLED ON TARGET ***"
+    ssh $1 'test -d ~/PirBlaster'
+    if [[ $? -ne 0 ]]
+    then
+        echo -e "\e[1;124mSERVICE NOT INSTALLED ON TARGET"
+        exit 1
+    fi
+    echo -e "\e[1;121mSERVICE INSTALLED ON TARGET"
+    return 0
+}
+
+function deployNewSrc() {
+    echo -e "\e[1;123m*** DEPLOYING NEW SOURCE CODE ***"
+    rsync -avz ./ $1:PirBlaster
+    if [[ $? -ne 0 ]]
+    then
+        echo -e "\e[1;124mSOURCE DEPLOYMENT FAILED"
+        exit 1
+    fi
+    echo -e "\e[1;121mNEW SOURCE CODE DEPLOYED"
+    return 0
+}
+
+function restartService() {
+    echo -e "\e[1;123m*** RESTARTING SERVICE ***"
+    ssh $1 'sudo systemctl restart pirblaster.service'
+    if [[ $? -ne 0 ]]
+    then
+        echo -e "\e[1;124mSERVICE RESTART FAILED"
+        exit 1
+    fi
+    echo -e "\e[1;121mSERVICE RESTARTED"
+    return 0
+}
+
+if [[ "$#" -ne 1 ]]
+then
+    echo -e "\e[1;124mBAD NUMBER OF ARGUMENT!!!"
+    exit 1
+fi
+
+isServiceInstalled $1
+if [[ $? -ne 0 ]]
+then
+    exit 1
+fi
+
+deployNewSrc $1
+if [[ $? -ne 0 ]]
+then
+    exit 1
+fi
+
+restartService $1
+if [[ $? -ne 0 ]]
+then
+    exit 1
+fi
+
+echo -e "\e[1;121mDEPLOYMENT DONE"
