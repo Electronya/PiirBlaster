543a68e6e87b3f7dc1d82aa92b1d39d7dc3374fc
jbacon <julien.bacon.1@gmail.com>
2021-06-13 12:39:37 -0400

    the constructor must instanciate Config

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,26 @@
 from unittest import TestCase
-# from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch
 
 import os
 import sys
 sys.path.append(os.path.abspath('./src'))
 
-from app import App     # noqa: 402
+from app import App                     # noqa: E402
+from config import Config               # noqa: 402
+from device import DeviceManager        # noqa: 402
 
 
 class TestApp(TestCase):
     """
     The App class test cases.
     """
-    def test_conctructor(self):
+    @patch('config.Config.__init__', autospec=True)
+    def test_conctructorLoadConfig(self, mockedConfigConst):
         """
-        Test the constructor.
+        The constructor must load the configuration by creating a new
+        instance of the Config object.
         """
-        self.assertTrue(True, 'prout.')
+        mockedConfigConst.return_value = None
+        with patch.object(DeviceManager, '__init__', lambda x, y, z: None):
+            app = App()                                         # noqa: F841
+            self.assertTrue(mockedConfigConst.called)
