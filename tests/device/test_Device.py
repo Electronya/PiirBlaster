38f4c09e205b7e904536a4b656e4ca18393b1836
jbacon <julien.bacon.1@gmail.com>
2021-06-20 14:00:11 -0400

    adding _initMqttClient test cases

--- tests/device/test_Device.py
+++ tests/device/test_Device.py
@@ -59,17 +59,17 @@ class TestDevice(TestCase):
         The constructor must create a new command set based on the device
         configuration when the isNew flag is True.
         """
-        mockedClient.side_effect = self.mockedClient
-        mockedCmdSet.side_effect = self.mockedCmdSet
+        mockedClient.side_effect = [self.mockedClient]
+        mockedCmdSet.side_effect = [self.mockedCmdSet]
         name = self.deviceConfig['commandSet']['model']
         emitter = self.deviceConfig['commandSet']['emitterGpio']
         receiver = self.deviceConfig['commandSet']['receiverGpio']
         description = self.deviceConfig['commandSet']['description']
         device = Device(logging, self.mockedAppConfig,      # noqa: F841
                         self.deviceConfig, isNew=True)
-        self.mockedCmdSet.assert_called_once_with(name, emitter_gpio=emitter,
-                                                  receiver_gpio=receiver,
-                                                  description=description)
+        mockedCmdSet.assert_called_once_with(name, emitter_gpio=emitter,
+                                             receiver_gpio=receiver,
+                                             description=description)
 
     @patch('device.Device.CommandSet.load')
     @patch('device.Device.mqtt.Client')
@@ -78,7 +78,7 @@ class TestDevice(TestCase):
         The constructor must load an existing command set when the
         isNew flag is Flase.
         """
-        mockedClient.side_effect = self.mockedClient
+        mockedClient.side_effect = [self.mockedClient]
         manufacturer = self.deviceConfig['commandSet']['manufacturer']
         model = self.deviceConfig['commandSet']['model']
         device = Device(logging, self.mockedAppConfig,      # noqa: F841
@@ -96,8 +96,8 @@ class TestDevice(TestCase):
         The constructor must initialize the MQTT client with the appropriate
         broker/user information.
         """
-        mockedClient.side_effect = self.mockedClient
-        mockedCmdSet.side_effect = self.mockedCmdSet
+        mockedClient.side_effect = [self.mockedClient]
+        mockedCmdSet.side_effect = [self.mockedCmdSet]
         username = self.mockedAppConfig.getUserName()
         password = self.mockedAppConfig.getUserPassword()
         hostname = self.mockedAppConfig.getBrokerHostname()
@@ -106,3 +106,96 @@ class TestDevice(TestCase):
                         self.deviceConfig)
         mockedClientInit.assert_called_once_with(username, password,
                                                  hostname, port)
+
+    @patch('device.Device.CommandSet')
+    @patch('device.Device.mqtt.Client')
+    def test__initMqttClientClientId(self, mockedClient, mockedCmdset):
+        """
+        The _initMqttClient (called by the constructor) method must initialize
+        the MQTT client with the correct client ID.
+        """
+        mockedClient.side_effect = [self.mockedClient]
+        mockedCmdset.side_effect = [self.mockedCmdSet]
+        device = Device(logging, self.mockedAppConfig,      # noqa: F841
+                        self.deviceConfig)
+        mockedClient.assert_called_once_with(client_id=f"{self.deviceConfig['location']}."      # noqa: E501
+                                             f"{self.deviceConfig['name']}")
+
+    @patch('device.Device.CommandSet')
+    @patch('device.Device.mqtt.Client')
+    def test__initMqttClientEventCallbacks(self, mockedClient, mockedCmdSet):
+        """
+        The _initMqttClient (called by the constructor) method must initialize
+        the client event callbacks.
+        """
+        mockedClient.side_effect = [self.mockedClient]
+        mockedCmdSet.side_effect = [self.mockedCmdSet]
+        device = Device(logging, self.mockedAppConfig,
+                        self.deviceConfig)
+        self.assertEqual(device.client.on_connect, device._on_connect,
+                         'Device _initClient method failed to initialize'
+                         'the client event callbacks.')
+        self.assertEqual(device.client.on_disconnect, device._on_disconnect,
+                         'Device _initClient method failed to initialize'
+                         'the client event callbacks.')
+        self.assertEqual(device.client.on_message, device._on_message,
+                         'Device _initClient method failed to initialize'
+                         'the client event callbacks.')
+        self.assertEqual(device.client.on_publish, device._on_publish,
+                         'Device _initClient method failed to initialize'
+                         'the client event callbacks.')
+        self.assertEqual(device.client.on_subscribe, device._on_subscribe,
+                         'Device _initClient method failed to initialize'
+                         'the client event callbacks.')
+        self.assertEqual(device.client.on_log, device._on_log,
+                         'Device _initClient method failed to initialize'
+                         'the client event callbacks.')
+
+    @patch('device.Device.CommandSet')
+    @patch('device.Device.mqtt.Client')
+    def test__initMqttClientLastWill(self, mockedClient, mockedCmdSet):
+        """
+        The _initMqttClient (called by the constructor) method must initialize
+        the client last will.
+        """
+        mockedClient.side_effect = [self.mockedClient]
+        mockedCmdSet.side_effect = [self.mockedCmdSet]
+        device = Device(logging, self.mockedAppConfig,
+                        self.deviceConfig)
+        willTopic = f"{device.baseTopic}{device.STATUS_TOPIC}"
+        qos = self.deviceConfig['lastWill']['qos']
+        retain = self.deviceConfig['lastWill']['retain']
+        self.mockedClient.will_set.assert_called_once_with(willTopic,
+                                                           device.OFFLINE_MSG,
+                                                           qos, retain)
+
+    @patch('device.Device.CommandSet')
+    @patch('device.Device.mqtt.Client')
+    def test__initMqttClientUserPassword(self, mockedClient, mockedCmdSet):
+        """
+        The _initMqttClient (called by the constructor) method must initialize
+        the client username and password.
+        """
+        mockedClient.side_effect = [self.mockedClient]
+        mockedCmdSet.side_effect = [self.mockedCmdSet]
+        device = Device(logging, self.mockedAppConfig,      # noqa: F841
+                        self.deviceConfig)
+        username = self.mockedAppConfig.getUserName()
+        password = self.mockedAppConfig.getUserPassword()
+        self.mockedClient.username_pw_set.assert_called_once_with(username,
+                                                                  password)
+
+    @patch('device.Device.CommandSet')
+    @patch('device.Device.mqtt.Client')
+    def test__initMqttClientConnect(self, mockedClient, mockedCmdSet):
+        """
+        The _initMqttClient (called by the constructor) method must connect
+        the client to the appropriate broker.
+        """
+        mockedClient.side_effect = [self.mockedClient]
+        mockedCmdSet.side_effect = [self.mockedCmdSet]
+        device = Device(logging, self.mockedAppConfig,      # noqa: F841
+                        self.deviceConfig)
+        hostname = self.mockedAppConfig.getBrokerHostname()
+        port = self.mockedAppConfig.getBrokerPort()
+        self.mockedClient.connect.assert_called_once_with(hostname, port=port)
